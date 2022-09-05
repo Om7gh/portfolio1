0bd645079bc32174a14e896b8045e54b0fb07906
Om7gh <111459163+Om7gh@users.noreply.github.com>
2022-09-05 19:38:46 +0100

    Add files via upload

--- Javascript/services.js
+++ Javascript/services.js
@@ -5,69 +5,94 @@ myBtn.addEventListener("click", function () {
   myLists.classList.toggle("show");
 });
 
-
 //gsap
 
-const tl= gsap.timeline({
+const tl = gsap.timeline({
   defaults: {
-      ease: 'power1.out'
+    ease: "power1.out",
+  },
+});
+
+tl.fromTo(
+  "main",
+  {
+    opacity: "0",
+    transform: "scale(0)",
+    duration: 1.2,
+  },
+  {
+    opacity: "1",
+    transform: "scale(1)",
+    duration: 1.5,
   }
-});
+);
+tl.fromTo(
+  ".left",
+  {
+    opacity: "0",
+    transform: "scale(0)",
+    duration: 1.2,
+  },
+  {
+    opacity: "1",
+    transform: "scale(1)",
+    duration: 1.5,
+  }
+);
 
-tl.fromTo("main", {
-  opacity: "0",
-  transform: "scale(0)",
-  duration: 1.2
-}, {
-  opacity: "1",
-  transform: "scale(1)",
-  duration: 1.5,
-})
-tl.fromTo(".left", {
-  opacity: "0",
-  transform: "scale(0)",
-  duration: 1.2
-}, {
-  opacity: "1",
-  transform: "scale(1)",
-  duration: 1.5,
-})
+tl.fromTo(
+  ".title",
+  {
+    opacity: "0",
+    duration: 1,
+  },
+  {
+    opacity: "1",
+    duration: 1.5,
+    delay: 0.3,
+  }
+);
+tl.fromTo(
+  ".one",
+  {
+    opacity: "0",
+  },
+  {
+    opacity: "1",
+    duration: 0.3,
+  }
+);
+tl.fromTo(
+  ".two",
+  {
+    opacity: "0",
+  },
+  {
+    opacity: "1",
+    delay: 0.5,
+    duration: 0.3,
+  }
+);
+tl.fromTo(
+  ".three",
+  {
+    opacity: "0",
+  },
+  {
+    opacity: "1",
+    delay: 0.5,
+    duration: 0.3,
+  }
+);
 
-tl.fromTo(".title", {
-  opacity: "0",
-  duration: 1
-}, {
- opacity: '1',
-  duration: 1.5,
-  delay: .3
-})
-tl.fromTo(".one", {
-  opacity: '0',
-}, {
-  opacity: '1',
-  duration: .3,
-});
-tl.fromTo(".two", {
-  opacity: '0',
-}, {
-  opacity: '1',
-  delay: .5,
-  duration: .3,
-
-});
-tl.fromTo(".three", {
-  opacity: '0',
-}, {
-  opacity: '1',
-  delay: .5,
-  duration: .3,
-
-});
-
-tl.fromTo(".go-back-to-home", {
-  opacity: "0",
-  duration: 1
-}, {
-  opacity: "1",
-  duration: 1,
-})
+tl.fromTo(
+  ".go-back-to-home",
+  {
+    opacity: "0",
+    duration: 1,
+  },
+  {
+    opacity: "1",
+    duration: 1,
+  }
+);
